0e36fcd51c7d9395522ea164a1bad417026018a1
LNailer <perma_luka@yahoo.com>
2025-01-25 20:03:02 +1100

    added error state styling and msg. I'd say done - or ready for UAT.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const validEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ ;
+let inputEmail = document.getElementById('email');
 
-const inputEmail = document.getElementById('email');
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('prevent default is working');
     console.log('the email is ' + inputEmail.value);
+
+    const invalidWarning = document.getElementById('valid-required');
 
     if(validEmail.test(inputEmail.value)) {
       console.log("that's a valid email");
@@ -13,16 +16,14 @@ form.addEventListener('submit', (e) => {
       firstMsg.classList.add('hide');
       const secondMsg = document.getElementById('happy-msg');
       secondMsg.classList.remove('hide');
+      invalidWarning.classList.add('hide');
+      inputEmail.classList.remove('error-state');
     }
     else {
       console.log("that isn't a valid email");
+      invalidWarning.classList.remove('hide');
+      inputEmail.classList.add('error-state');
     }
-});
-
-const dButton = document.getElementById('dismiss');
-
-dButton.addEventListener('onClick', (e) => {
-  console.log('dismiss got clicked');
 });
 
 function dismiss(){
